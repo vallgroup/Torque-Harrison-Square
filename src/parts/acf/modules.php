b49a7bdc37d9093f4af25bc83a6d19a753006c27
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-13 15:51:02 -0600

    add text and image section acf fields

--- src/parts/acf/modules.php
+++ src/parts/acf/modules.php
@@ -7,6 +7,21 @@ if ( have_rows( $modules ) ):
   while ( have_rows( $modules ) ) : the_row();
 
     switch ( get_row_layout() ) {
+
+      case 'text_and_image' :
+
+        $anchor = get_sub_field('anchor');
+        $align = get_sub_field( 'align' );
+        $background_color = get_sub_field('background_color');
+        $title_text_size = get_sub_field('title_text_size');
+        $image = get_sub_field( 'image' );
+        $title = get_sub_field( 'title' );
+        $content = get_sub_field( 'content' );
+        $cta = get_sub_field('cta');
+
+
+
+        break;
     }
 
   endwhile;
