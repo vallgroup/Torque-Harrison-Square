be333bf6f85cf0ec24983c013f5ba68921dcb176
Mario Vallejo <mario@vallgroup.com>
2019-02-27 12:54:23 -0600

    using flexbox, set the images to cover fullheight while the text content remains aligned vertically

--- src/parts/acf/modules/text-and-image.php
+++ src/parts/acf/modules/text-and-image.php
@@ -7,25 +7,27 @@
   </div>
 
   <div class="content-wrapper" >
-    <?php if ($title) { ?>
-      <?php if ($title_text_size === 'large') { ?>
-        <h1><?php echo $title; ?></h1>
-      <?php } else { ?>
-        <h2><?php echo $title; ?></h2>
+    <div class="content-inner">
+      <?php if ($title) { ?>
+        <?php if ($title_text_size === 'large') { ?>
+          <h1><?php echo $title; ?></h1>
+        <?php } else { ?>
+          <h2><?php echo $title; ?></h2>
+        <?php } ?>
       <?php } ?>
-    <?php } ?>
 
-    <?php if ($content) { ?>
-      <div class="content"><?php echo $content; ?></div>
-    <?php } ?>
+      <?php if ($content) { ?>
+        <div class="content"><?php echo $content; ?></div>
+      <?php } ?>
 
-    <?php if ($cta) { ?>
-      <div class="cta-wrapper" >
-        <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
-          <button><?php echo $cta['title']; ?></button>
-        </a>
-      </div>
-    <?php } ?>
+      <?php if ($cta) { ?>
+        <div class="cta-wrapper" >
+          <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
+            <button><?php echo $cta['title']; ?></button>
+          </a>
+        </div>
+      <?php } ?>
+    </div>
   </div>
 
 </div>
